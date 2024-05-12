3c8283e091faed5717394a0e00501562a74f4a34
alexsamboy <alexsamboy@gmail.com>
2024-05-12 12:26:41 -0400

    ok

--- rss-feed/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'ca0297761aa61aaf2bdd');
--- rss-feed/build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'a98dd140af9f75031e30');
